41418b282fc8b8eacd2fc32e34255a7b243945dd
himanshutl <himanshu.techlead@gmail.com>
2024-02-15 21:37:46 +0530

    initial setup

---
+++ src/test/java/com/deliveryapp/restaurantservice/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryapp.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
